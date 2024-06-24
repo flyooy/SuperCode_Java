0d6689b9895a2316c8b544bdbeeb706cbe4ffe59
flyooy <flyooy@gmail.com>
2024-06-24 15:08:45 +0200

    Neue Ordner

---
+++ 1Day_Intro/EingabeUmdrehen.java
@@ -0,0 +1,6 @@
+public class EingabeUmdrehen {
+    public static void main(String[] args) {
+        System.out.println(args[2] + " " + args[1] + " " + args[0]);
+
+    }
+}
---
+++ 1Day_Intro/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String myName = "Yan";
+        System.out.println("Hello World!" + " " + myName);
+        System.out.println("Warum können Geister so schlecht lügen? - Weil man durch sie hindurchsehen kann.");
+
+    }
+}
