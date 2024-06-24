cdce0570bfe99469d1039654e80306ad7113a615
flyooy <flyooy@gmail.com>
2024-06-24 15:08:45 +0200

    Neue Ordner

---
+++ 1Day_Intro/FehlerFinden.java
@@ -0,0 +1,20 @@
+//1. 
+public class FehlerFinden {
+    public static void main(String[] args) {
+        System.out.println("Dieses Programm hat einen Fehler.");
+        System.out.println("Kannst du ihn finden?");
+    }
+
+    // 1. System.out.prrintln("Kannst du ihn finden?"); --> println
+}
+
+
+//2.
+// class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println(Hello, World!); 
+//     }
+// }
+
+// 1. class HelloWorld --> public class HelloWorld
+// 2. System.out.println(Hello, World!); --> System.out.println("Hello, World!"");
---
+++ 1Day_Intro/Variablen.java
@@ -0,0 +1,11 @@
+public class Variablen {
+    public static void main(String[] args) {
+    
+        String name = "Andreas";
+        char Buchstabe = 'A';
+        System.out.println(name);
+        System.out.println(Buchstabe);
+        int zahl = -100;
+        
+    }
+}
